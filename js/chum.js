74e157de3be371a787a22875963416ddd6a4895c
irysius <lestersy@hotmail.com>
2014-06-14 14:34:26 -0400

    Adding expect.  and duplicates test cases

--- js/chum.js
+++ js/chum.js
@@ -1,322 +1,331 @@
 (function (definition) {
-    var exports = definition();
-    window.chum = exports.chum;
+	var exports = definition();
+	window.chum = exports.chum;
 })(function () {
-    var selObject = 'data-chum-obj';
-    var selProp = 'data-chum-prop';
-    var selType = 'data-chum-type';
-    var showDebug = false;
-    var dictionary = {};
-    var events = {};
-    var customAccessors = {};
-    var radioNames = []; // Limit one property ever on grouped radios.
+	var selObject = 'data-chum-obj';
+	var selProp = 'data-chum-prop';
+	var selType = 'data-chum-type';
+	var showDebug = false;
+	var dictionary = {};
+	var events = {};
+	var customAccessors = {};
+	var radioNames = []; // Limit one property ever on grouped radios.
 
-    function getBasic($elem) {
-        return $elem.val();
-    }
-    function setBasic($elem, val) {
-        $elem.val(val);
-    }
-    function BasicAccessor($elem, change) {
-        var accessor = {
-            get: function () {
-                return getBasic($elem);
-            },
-            set: function (val) {
-                var before, after;
-                if (_.isFunction(change)) { before = accessor.get(); }
+	function getBasic($elem) {
+		return $elem.val();
+	}
+	function setBasic($elem, val) {
+		$elem.val(val);
+	}
+	function BasicAccessor($elem, change) {
+		var accessor = {
+			get: function () {
+				return getBasic($elem);
+			},
+			set: function (val) {
+				var before, after;
+				if (_.isFunction(change)) { before = accessor.get(); }
 
-                setBasic($elem, val);
+				setBasic($elem, val);
 
-                if (_.isFunction(change)) {
-                    after = accessor.get();
-                    change(before, after);
-                }
-            }
-        }
-        return accessor;
-    }
-    function getCheckbox($elem) {
-        return $elem[0].checked;
-    }
-    function setCheckbox($elem, checked) {
-        $elem[0].checked = checked;
-    }
-    function CheckboxAccessor($elem, change) {
-        var accessor = {
-            get: function () {
-                return getCheckbox($elem);
-            },
-            set: function (val) {
-                var before, after;
-                if (_.isFunction(change)) { before = accessor.get(); }
+				if (_.isFunction(change)) {
+					after = accessor.get();
+					change(before, after);
+				}
+			}
+		}
+		return accessor;
+	}
+	function getCheckbox($elem) {
+		return $elem[0].checked;
+	}
+	function setCheckbox($elem, checked) {
+		$elem[0].checked = checked;
+	}
+	function CheckboxAccessor($elem, change) {
+		var accessor = {
+			get: function () {
+				return getCheckbox($elem);
+			},
+			set: function (val) {
+				var before, after;
+				if (_.isFunction(change)) { before = accessor.get(); }
 
-                setCheckbox($elem, val);
+				setCheckbox($elem, val);
 
-                if (_.isFunction(change)) {
-                    after = accessor.get();
-                    change(before, after);
-                }
-            }
-        }
-        return accessor;
-    }
-    function getNumber($elem) {
-        return parseFloat($elem.val());
-    }
-    function setNumber($elem, val) {
-        var raw = parseFloat(val);
-        $elem.val(raw);
-    }
-    function NumberAccessor($elem, change) {
-        var accessor = {
-            get: function () {
-                return getNumber($elem);
-            },
-            set: function (val) {
-                var before, after;
-                if (_.isFunction(change)) { before = accessor.get(); }
+				if (_.isFunction(change)) {
+					after = accessor.get();
+					change(before, after);
+				}
+			}
+		}
+		return accessor;
+	}
+	function getNumber($elem) {
+		return parseFloat($elem.val());
+	}
+	function setNumber($elem, val) {
+		var raw = parseFloat(val);
+		$elem.val(raw);
+	}
+	function NumberAccessor($elem, change) {
+		var accessor = {
+			get: function () {
+				return getNumber($elem);
+			},
+			set: function (val) {
+				var before, after;
+				if (_.isFunction(change)) { before = accessor.get(); }
 
-                setNumber($elem, val);
+				setNumber($elem, val);
 
-                if (_.isFunction(change)) {
-                    after = accessor.get();
-                    change(before, after);
-                }
-            }
-        }
-        return accessor;
-    }
-    function getRadio($elem) {
-        var name = $elem.attr('name');
-        return $('input[type="radio"][name="' + name + '"]:checked').val();
-    }
-    function setRadio($elem, val) {
-        var name = $elem.attr('name');
-        var radioes = $('input[type="radio"][name="' + name + '"]');
-        _.each(radioes, function (radio) {
-            var $radio = $(radio);
-            if ($radio.val() == val) {
-                $radio[0].checked = true;
-            } else {
-                $radio[0].checked = false;
-            }
-        })
-    }
-    function RadioAccessor($elem, change) {
-        var accessor = {
-            get: function () {
-                return getRadio($elem);
-            },
-            set: function (val) {
-                var before, after;
-                if (_.isFunction(change)) { before = accessor.get(); }
+				if (_.isFunction(change)) {
+					after = accessor.get();
+					change(before, after);
+				}
+			}
+		}
+		return accessor;
+	}
+	function getRadio($elem) {
+		var name = $elem.attr('name');
+		return $('input[type="radio"][name="' + name + '"]:checked').val();
+	}
+	function setRadio($elem, val) {
+		var name = $elem.attr('name');
+		var radioes = $('input[type="radio"][name="' + name + '"]');
+		_.each(radioes, function (radio) {
+			var $radio = $(radio);
+			if ($radio.val() == val) {
+				$radio[0].checked = true;
+			} else {
+				$radio[0].checked = false;
+			}
+		})
+	}
+	function RadioAccessor($elem, change) {
+		var accessor = {
+			get: function () {
+				return getRadio($elem);
+			},
+			set: function (val) {
+				var before, after;
+				if (_.isFunction(change)) { before = accessor.get(); }
 
-                setRadio($elem, val);
+				setRadio($elem, val);
 
-                if (_.isFunction(change)) {
-                    after = accessor.get();
-                    change(before, after);
-                }
-            }
-        }
-        return accessor;
-    }
+				if (_.isFunction(change)) {
+					after = accessor.get();
+					change(before, after);
+				}
+			}
+		}
+		return accessor;
+	}
 
-    function createObject($unit) {
-        var name = $unit.attr(selObject);
-        if (!name) { return null; }
-        showDebug && console.log('---', name);
-        var unit = {};
-        var events = {};
-        var propNames = [];
-        var props = $unit.find('[' + selProp + ']');
-        _.each(props, function (prop) {
-            var $prop = $(prop);
-            var propName = $prop.attr(selProp);
-            if (!!propName) {
-                if (!_.contains(propNames, propName)) {
-                    propNames.push(propName);
-                } else {
-                    showDebug && console.log(name, "'s", propName, 'violated uniqueness constraint');
-                    propNames = _.remove(propNames, propName);
-                }
-            }
-        })
+	function createObject($unit) {
+		var name = $unit.attr(selObject);
+		if (!name) { return null; }
+		showDebug && console.log('---', name);
+		var unit = {};
+		var events = {};
+		var propNames = [];
+		var propNameViolations = [];
+		var props = $unit.find('[' + selProp + ']');
+		_.each(props, function (prop) {
+			var $prop = $(prop);
+			var propName = $prop.attr(selProp);
+			if (!!propName) {
+				console.log(propNames);
+				if (!_.contains(propNames, propName)) {
+					propNames.push(propName);
+				} else {
+					console.log(name + "'s", propName, 'violated uniqueness constraint');
+					if (!_.contains(propNameViolations, propName)) {
+						propNameViolations.push(propName);
+					}
+				}
+			}
+		})
+
+		_.each(propNameViolations, function (violation) {
+			propNames = _.without(propNames, violation);
+		})
 
 
-        showDebug && console.log('properties found:', propNames.length);
+		showDebug && console.log('properties found:', propNames.length);
 
-        _.each(propNames, function (propName) {
-            var $prop = $unit.find('[' + selProp + '="' + propName + '"]');
-            var accessors = null;
-            var skip = false;
-            if ($prop.is('[' + selType + ']')) {
-                showDebug && console.log('createObject - custom');
-                var type = $prop.attr(selType);
-                if (!!customAccessors[type]) {
-                    showDebug && console.log('createObject - custom, found');
-                    accessors = new customAccessors[type]($prop, function (before, after) {
-                        if (!!events['change'] && events['change'].length > 0) {
-                            _.each(events['change'], function (callback) {
-                                callback(unit, propName, before, after);
-                            })
-                        }
-                    });
-                }
-            }
+		_.each(propNames, function (propName) {
+			var $prop = $unit.find('[' + selProp + '="' + propName + '"]');
+			var accessors = null;
+			var skip = false;
 
-            if (!accessors && $prop.is('[type="checkbox"]')) {
-                showDebug && console.log('createObject - checkbox');
-                accessors = new CheckboxAccessor($prop, function (before, after) {
-                    if (!!events['change'] && events['change'].length > 0) {
-                        _.each(events['change'], function (callback) {
-                            callback(unit, propName, before, after);
-                        })
-                    }
-                });
-            }
+			if ($prop.is('[' + selType + ']')) {
+				showDebug && console.log('createObject - custom');
+				var type = $prop.attr(selType);
+				if (!!customAccessors[type]) {
+					showDebug && console.log('createObject - custom, found');
+					accessors = new customAccessors[type]($prop, function (before, after) {
+						if (!!events['change'] && events['change'].length > 0) {
+							_.each(events['change'], function (callback) {
+								callback(unit, propName, before, after);
+							})
+						}
+					});
+				}
+			}
 
-            if (!accessors && $prop.is('[type="number"]')) {
-                showDebug && console.log('createObject - number');
-                accessors = new NumberAccessor($prop, function (before, after) {
-                    if (!!events['change'] && events['change'].length > 0) {
-                        _.each(events['change'], function (callback) {
-                            callback(unit, propName, before, after);
-                        })
-                    }
-                });
-            }
+			if (!accessors && $prop.is('[type="checkbox"]')) {
+				showDebug && console.log('createObject - checkbox');
+				accessors = new CheckboxAccessor($prop, function (before, after) {
+					if (!!events['change'] && events['change'].length > 0) {
+						_.each(events['change'], function (callback) {
+							callback(unit, propName, before, after);
+						})
+					}
+				});
+			}
 
-            if (!accessors && $prop.is('[type="radio"]')) {
-                showDebug && console.log('createObject - radio');
-                var radioName = $prop.attr('name');
-                var unique = true;
-                _.each(radioNames, function (item) {
-                    if (item.name == radioName) {
-                        item.duplicate = true;
-                        propNames = _.remove(propNames, propName);
-                        unique = false;
-                    }
-                })
-                if (unique) {
-                    radioNames.push({
-                        name: radioName,
-                        prop: propName,
-                        container: name,
-                        duplicate: false
-                    });
-                    accessors = new RadioAccessor($prop, function (before, after) {
-                        if (!!events['change'] && events['change'].length > 0) {
-                            _.each(events['change'], function (callback) {
-                                callback(unit, propName, before, after);
-                            })
-                        }
-                    });
-                } else {
-                    skip = true;
-                }
-            }
+			if (!accessors && $prop.is('[type="number"]')) {
+				showDebug && console.log('createObject - number');
+				accessors = new NumberAccessor($prop, function (before, after) {
+					if (!!events['change'] && events['change'].length > 0) {
+						_.each(events['change'], function (callback) {
+							callback(unit, propName, before, after);
+						})
+					}
+				});
+			}
 
-            if (!accessors) {
-                showDebug && console.log('createObject - default');
-                accessors = new BasicAccessor($prop, function (before, after) {
-                    if (!!events['change'] && events['change'].length > 0) {
-                        _.each(events['change'], function (callback) {
-                            console.log(callback);
-                            callback(unit, propName, before, after);
-                        })
-                    }
-                });
-            }
+			if (!accessors && $prop.is('[type="radio"]')) {
+				showDebug && console.log('createObject - radio');
+				var radioName = $prop.attr('name');
+				var unique = true;
+				_.each(radioNames, function (item) {
+					if (item.name == radioName) {
+						item.duplicate = true;
+						propNames = _.without(propNames, propName);
+						unique = false;
+					}
+				})
+				if (unique) {
+					radioNames.push({
+						name: radioName,
+						prop: propName,
+						container: name,
+						duplicate: false
+					});
+					accessors = new RadioAccessor($prop, function (before, after) {
+						if (!!events['change'] && events['change'].length > 0) {
+							_.each(events['change'], function (callback) {
+								callback(unit, propName, before, after);
+							})
+						}
+					});
+				} else {
+					skip = true;
+				}
+			}
 
-            try {
-                if (!skip) {
-                    Object.defineProperty(unit, propName,
+			if (!accessors) {
+				showDebug && console.log('createObject - default');
+				accessors = new BasicAccessor($prop, function (before, after) {
+					if (!!events['change'] && events['change'].length > 0) {
+						_.each(events['change'], function (callback) {
+							console.log(callback);
+							callback(unit, propName, before, after);
+						})
+					}
+				});
+			}
+
+			try {
+				if (!skip) {
+					Object.defineProperty(unit, propName,
                     {
-                        get: accessors.get,
-                        set: accessors.set
+                    	get: accessors.get,
+                    	set: accessors.set
                     });
-                }
-            } catch (err) {
-                showDebug && console.log('Could not define property', propName, 'for', name);
-            }
-        })
+				}
+			} catch (err) {
+				showDebug && console.log('Could not define property', propName, 'for', name);
+			}
+		})
 
-        unit.serialize = function () {
-            var data = {};
-            _.each(propNames, function (propName) {
-                data[propName] = unit[propName];
-            })
-            return data;
-        }
+		unit.serialize = function () {
+			var data = {};
+			_.each(propNames, function (propName) {
+				data[propName] = unit[propName];
+			})
+			return data;
+		}
 
-        unit.on = function (event, callback) {
-            if (!events[event] || !_.isArray(events[event])) {
-                events[event] = [];
-            }
-            events[event].push(callback);
-            showDebug && console.log(events[event]);
-        }
+		unit.on = function (event, callback) {
+			if (!events[event] || !_.isArray(events[event])) {
+				events[event] = [];
+			}
+			events[event].push(callback);
+			showDebug && console.log(events[event]);
+		}
 
-        unit.off = function (event, callback) {
-            events[event] = _.remove(events[event], callback);
-            showDebug && console.log(events[event]);
-        }
+		unit.off = function (event, callback) {
+			events[event] = _.remove(events[event], callback);
+			showDebug && console.log(events[event]);
+		}
 
-        _.each(radioNames, function (item) {
-            if (item.duplicate && item.container == name) {
-                propNames = _.remove(propNames, item.prop);
-                delete unit[item.prop];
-            }
-        })
+		_.each(radioNames, function (item) {
+			if (item.duplicate && item.container == name) {
+				propNames = _.without(propNames, item.prop);
+				delete unit[item.prop];
+			}
+		})
 
-        unit.props = propNames;
+		unit.props = propNames;
 
-        dictionary[name] = unit;
-        return dictionary[name];
-    }
+		dictionary[name] = unit;
+		return dictionary[name];
+	}
 
-    function registerAccessor(typeName, accessor) {
-        customAccessors[typeName] = accessor;
-    }
+	function registerAccessor(typeName, accessor) {
+		customAccessors[typeName] = accessor;
+	}
 
-    function rescan() {
-        var units = $('[' + selObject + ']');
-        _.each(units, function (unit) {
-            createObject($(unit));
-        })
-        if (!!events['rescanned'] && events['rescanned'].length > 0) {
-            _.each(events['rescanned'], function (callback) {
-                callback();
-            })
-        }
-    }
+	function rescan() {
+		var units = $('[' + selObject + ']');
+		_.each(units, function (unit) {
+			createObject($(unit));
+		})
+		if (!!events['rescanned'] && events['rescanned'].length > 0) {
+			_.each(events['rescanned'], function (callback) {
+				callback();
+			})
+		}
+	}
 
-    function on(event, callback) {
-        if (!events[event] || !_.isArray(events[event])) {
-            events[event] = [];
-        }
-        events[event].push(callback);
-    }
+	function on(event, callback) {
+		if (!events[event] || !_.isArray(events[event])) {
+			events[event] = [];
+		}
+		events[event].push(callback);
+	}
 
-    function off(event, callback) {
-        events[event] = _.remove(events[event], callback);
-    }
+	function off(event, callback) {
+		events[event] = _.remove(events[event], callback);
+	}
 
-    var chum = {
-        rescan: rescan,
-        registerType: registerAccessor,
-        items: dictionary,
-        on: on,
-        off: off
-    }
+	var chum = {
+		rescan: rescan,
+		registerType: registerAccessor,
+		items: dictionary,
+		on: on,
+		off: off
+	}
 
-    $(function () {
-        chum.rescan();
-    })
+	$(function () {
+		chum.rescan();
+	})
 
-    return {
-        chum: chum
-    };
+	return {
+		chum: chum
+	};
 })
---
+++ tests/js/duplicates.js
@@ -0,0 +1,107 @@
+describe('chum.duplicates', function () {
+	before(function (done) {
+		$(function () { done(); })
+	})
+
+	describe('the control object', function () {
+		it('is expected to exist on items', function (done) {
+			var items = chum.items;
+			expect(items).to.have.property('control');
+			done();
+		});
+		it('is expected to have a text property with the value "control"', function (done) {
+			var control = chum.items.control;
+			expect(control.text).to.equal('control');
+			done();
+		})
+	})
+	describe('duplicate object names', function () {
+		it('are expected to form an array', function (done) {
+			var item = chum.items.item;
+			expect(item).to.be.an.Array;
+			expect(item.length).to.equal(3);
+			done();
+		})
+		describe('item1', function () {
+			it('is expected to have a text property', function (done) {
+				var item1 = chum.items.item[0];
+				expect(item1).to.have.property('text');
+				expect(item1.text).to.equal('001');
+			})
+			it('that is expected to be settable', function (done) {
+				var item1 = chum.items.item[0];
+				item1.text = '101';
+				expect(item1.text).to.equal('101');
+
+				var $item = $($('[data-chum-obj="item"]')[0]);
+				var $prop = $($item.find('[data-chum-prop="text"]')[0]);
+				expect($prop.val()).to.equal('101');
+			})
+		})
+		describe('item2', function () {
+			it('is expected to have a property named boolean', function (done) {
+				var item2 = chum.items.item[1];
+				expect(item2).to.have.property('boolean');
+				expect(item2.boolean).to.equal(true);
+			})
+			it('that is expected to be settable', function (done) {
+				var item2 = chum.items.item[1];
+				item2.boolean = false;
+				expect(item2.boolean).to.equal(false);
+
+				var $item = $($('[data-chum-obj="item"]')[1]);
+				var prop = $item.find('[data-chum-prop="boolean"]')[0];
+				expect(prop.checked).to.equal(false);
+			})
+		})
+		describe('item3', function () {
+			it('is expected to have a text property', function (done) {
+				var item3 = chum.items.item[2];
+				expect(item3).to.have.property('text');
+				expect(item3.text).to.equal('002');
+			})
+			it('that is expected to be settable', function (done) {
+				var item3 = chum.items.item[2];
+				item3.text = '102';
+				expect(item1.text).to.equal('102');
+
+				var $item = $($('[data-chum-obj="item"]')[2]);
+				var $prop = $($item.find('[data-chum-prop="text"]')[0]);
+				expect($prop.val()).to.equal('102');
+			})
+		})
+	})
+
+	describe('duplicate property names', function () {
+		describe('on doodadA', function () {
+			it('is expected not to exist', function (done) {
+				var doodadA = chum.items.doodadA;
+				expect(doodadA.props.length).to.equal(0);
+				done();
+			})
+		})
+		describe('on doodadB', function () {
+			it('is expected not to exist', function (done) {
+				var doodadB = chum.items.doodadB;
+				expect(doodadB.props.length).to.equal(0);
+				done();
+			})
+		})
+		describe('on doodadC', function () {
+			it('is expected not to exist', function (done) {
+				var doodadC = chum.items.doodadC;
+				expect(doodadC.props.length).to.equal(0);
+				done();
+			})
+		})
+		describe('on doodadD', function () {
+			it('is expected not to exist', function (done) {
+				var doodadD = chum.items.doodadD;
+				expect(doodadD.props.length).to.equal(1);
+				expect(doodadD).to.have.property('text');
+				expect(doodadD.text).to.equal('blue');
+				done();
+			})
+		})
+	})
+})
